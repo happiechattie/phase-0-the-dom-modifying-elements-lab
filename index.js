df69a8ad7fdc1b0be4e35e925f2ead823f7b4350
chattie <christuaik@gmail.com>
2023-02-19 21:29:40 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+newHeader.id = 'victory';
+newHeader.textContent = "CHATTIE is the champion";
